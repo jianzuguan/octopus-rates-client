de218f9159b8b4c4e4c01e71882350b39193b425
Jianzu Guan <git@jzg.fastmail.com>
2024-10-26 22:49:20 +0100

    Refactor base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/octopus-rates-client/',
   resolve: {
     alias: {
       "@": "/src",
